50f252daa247bf54b08e5f70c319cb82ff13abd9
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2023-07-19 04:55:26 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// Create web server
+app.get('/api/comments', function(req, res) {
+  res.json(comments);
+});
+
+app.post('/api/comments', function(req, res) {
+  var comment = {
+    id: Date.now(),
